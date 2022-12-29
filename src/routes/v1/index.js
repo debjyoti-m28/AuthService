d9e14e10d3440371d01e0a8861aafc98f3e1808f
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2022-12-30 00:54:09 +0530

    Added auth middlewares for signup and signin

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const UserController = require("../../controllers/user-controller");
+const AuthMiddleware = require("../../middlewares/auth-middlewares");
 
 const router = express.Router();
 
-router.post('/signup', UserController.create);
-router.post('/signin', UserController.signIn);
+router.post('/signup', AuthMiddleware.validateUserAuth, UserController.create);
+router.post('/signin', AuthMiddleware.validateUserAuth, UserController.signIn);
 
 module.exports = router;
