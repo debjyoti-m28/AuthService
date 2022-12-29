c95c4d66e2f96f520295e6b97c4518e1165185b8
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2022-12-29 21:40:42 +0530

    Added get user by id in repository layer

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,7 +1,8 @@
 const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
 dotenv.config();
 
 module.exports = {
     PORT: process.env.PORT,
-    SALT: process.env.SALT
+    SALT: bcrypt.genSaltSync(10)
 }
